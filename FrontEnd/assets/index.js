6d4583b48064eed1b759ee12fde60e45b93241ca
Cris0338 <cris0338@gmail.com>
2023-11-24 11:28:59 +0100

    Fix Gallery & Filters

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,94 +1,126 @@
-// import { getAllData } from "./api.js";
+// Importation des données depuis l'API
+import { getAllData } from "./api.js";
 
-// const project = await getAllData();
-// console.log(project);
+// Création du tableau de tous les projets
+let allProjects = [];
 
+// Fonction pour afficher les projets dans la galerie
+function renderProjects(projects) {
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = '';
 
-        // Importdes données API
-        import { getAllData } from "./api.js";
+    projects.forEach(project => {
+        // Création de l'élément figure
+        const projectElement = document.createElement('figure');
 
-        // creation du tableau
-        let allProjects = [];
+        const imgElement = document.createElement('img');
+        imgElement.src = project.imageUrl;
+        imgElement.alt = project.title;
 
-        // Tableau catégories
-        const desiredCategories = ['Objets', 'Appartements', 'Hotels & restaurants'];
+        // Création du titre
+        const titleElement = document.createElement('figcaption');
+        titleElement.textContent = project.title;
 
-        // Fonction d'ajout image e titre a la gallerie
-        function renderProjects(projects) {
-            const gallery = document.querySelector('.gallery');
-            gallery.innerHTML = '';
+        // Ajout de l'image et du titre
+        projectElement.appendChild(imgElement);
+        projectElement.appendChild(titleElement);
 
-            projects.forEach(project => {
-                // Creation image
-                const projectElement = document.createElement('figure');
+        // Ajout du projet à la galerie
+        gallery.appendChild(projectElement);
+    });
+}
 
-                const imgElement = document.createElement('img');
-                imgElement.src = project.imageUrl;
-                imgElement.alt = project.title;
+// Fonction pour créer les boutons de filtre
+function createFilterButtons(categories) {
+    const filterButtonsContainer = document.querySelector('.filter-buttons');
 
-                // Creation titre
-                const titleElement = document.createElement('figcaption');
-                titleElement.textContent = project.title;
+    // Ajout du bouton "Tous" avec la classe active par défaut
+    const allButton = document.createElement('button');
+    allButton.textContent = 'Tous';
+    allButton.classList.add('active'); // Ajout de la classe active
+    allButton.onclick = () => filterProjects('all', allButton);
+    filterButtonsContainer.appendChild(allButton);
 
-                // Ajout img e titre
-                projectElement.appendChild(imgElement);
-                projectElement.appendChild(titleElement);
+    // Ajout des boutons de catégorie obtenus dynamiquement
+    categories.forEach(categoryName => {
+        const categoryButton = document.createElement('button');
+        categoryButton.textContent = categoryName;
+        categoryButton.onclick = () => filterProjects(categoryName, categoryButton);
+        filterButtonsContainer.appendChild(categoryButton);
 
-                // Ajout img a la gallerie
-                gallery.appendChild(projectElement);
-            });
-        }
+        // Ajout de la classe non-active et couleur par défaut
+        categoryButton.classList.add('non-active');
+        categoryButton.style.color = '#1D6154';
 
-        // Function pour créer les boutons des filtres
-        function createFilterButtons() {
-            const filterButtonsContainer = document.querySelector('.filter-buttons');
-
-            // Ajout btn "tous"
-            const allButton = document.createElement('button');
-            allButton.textContent = 'Tous';
-            allButton.onclick = () => filterProjects('all', allButton);
-            filterButtonsContainer.appendChild(allButton);
-
-            // Ajout btns categories
-            desiredCategories.forEach(categoryName => {
-                const categoryButton = document.createElement('button');
-                categoryButton.textContent = categoryName;
-                categoryButton.onclick = () => filterProjects(categoryName, categoryButton);
-                filterButtonsContainer.appendChild(categoryButton);
-            });
-        }
-
-        // Function filtre
-        function filterProjects(categoryName, button) {
-            // Enlever la classe "active" et ajouter "non-active"
-            document.querySelectorAll('.filter-buttons button').forEach(btn => {
-                btn.classList.remove('active');
-                btn.classList.add('non-active');
-            });
-
-            // Ajouter la classe "active" enlever "non-active" seulement bouton selectionné
-            button.classList.add('active');
-            button.classList.remove('non-active');
-
-            if (categoryName === 'all') {
-                renderProjects(allProjects); //montre tous
-            } else {
-                const filteredProjects = allProjects.filter(project => project.category.name === categoryName);
-                renderProjects(filteredProjects); // montre seulement la cat selectionnée
+        // Event listener pour gérer le style pendant l'hover pour les boutons non actifs
+        categoryButton.addEventListener('mouseover', () => {
+            if (!categoryButton.classList.contains('active')) {
+                categoryButton.style.color = 'white';
             }
-        }
+        });
 
-        // Fonction qui charge le tout
-        async function loadProjects() {
-            try {
-                allProjects = await getAllData();
-
-                createFilterButtons();
-                renderProjects(allProjects);
-            } catch (error) {
-                console.error('Erreur dans le chargement des projets:', error);
+        // Réinitialisation de la couleur du texte après l'hover pour les boutons non actifs
+        categoryButton.addEventListener('mouseout', () => {
+            if (!categoryButton.classList.contains('active')) {
+                categoryButton.style.color = '#1D6154';
             }
-        }
+        });
+    });
 
-        // Appel à la fonction
-        loadProjects();
+    // Gestion spécifique de l'hover pour le bouton "Tous"
+    allButton.addEventListener('mouseover', () => {
+        if (!allButton.classList.contains('active')) {
+            allButton.style.color = 'white';
+        }
+    });
+
+    // Réinitialisation de la couleur du texte après l'hover pour le bouton "Tous"
+    allButton.addEventListener('mouseout', () => {
+        if (!allButton.classList.contains('active')) {
+            allButton.style.color = '#1D6154';
+        }
+    });
+}
+
+// Fonction pour filtrer les projets par catégorie
+function filterProjects(categoryName, button) {
+    // Suppression de la classe "active" et ajout de "non-active"
+    document.querySelectorAll('.filter-buttons button').forEach(btn => {
+        btn.classList.remove('active');
+        btn.classList.add('non-active');
+        // Réinitialisation de la couleur du texte pour tous les boutons
+        btn.style.color = '#1D6154';
+    });
+
+    // Ajout de la classe "active" uniquement au bouton sélectionné
+    button.classList.add('active');
+    // Changement de la couleur du texte pour le bouton sélectionné
+    button.style.color = 'white';
+
+    if (categoryName === 'all') {
+        renderProjects(allProjects); // Affiche tous les projets
+    } else {
+        const filteredProjects = allProjects.filter(project => project.category.name === categoryName);
+        renderProjects(filteredProjects); // Affiche uniquement la catégorie sélectionnée
+    }
+}
+
+// Fonction pour charger tous les projets
+async function loadProjects() {
+    try {
+        allProjects = await getAllData();
+
+        // Obtenir les catégories uniques des projets
+        const uniqueCategories = [...new Set(allProjects.map(project => project.category.name))];
+
+        // Créer les boutons de filtre en utilisant les catégories obtenues dynamiquement
+        createFilterButtons(uniqueCategories);
+
+        renderProjects(allProjects);
+    } catch (error) {
+        console.error('Erreur dans le chargement des projets:', error);
+    }
+}
+
+// Appel à la fonction de chargement des projets
+loadProjects();
