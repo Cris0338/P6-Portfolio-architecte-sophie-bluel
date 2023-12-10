b35b50bcd564321d42e2733e0aded89d7f05e367
Cris0338 <cris0338@gmail.com>
2023-12-10 22:48:29 +0100

    fix error message

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,4 +1,5 @@
-let classefetch = null; // Initialise la variable au début du fichier
+// Initialise la variable au début du fichier
+let classefetch = null;
 
 // Écouteur d'événement pour le formulaire de connexion
 loginForm.addEventListener('submit', function (event) {
@@ -6,32 +7,37 @@ loginForm.addEventListener('submit', function (event) {
 
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
+    let verifierDonnees = document.getElementById('verifierDonnees');
+    verifierDonnees.style.color = 'red';
+    verifierDonnees.style.fontSize = '20px';
+    verifierDonnees.style.whiteSpace = 'nowrap';
 
-    // Vérifie si les champs email et mot de passe sont remplis
-    if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
-        // test
-        console.log('Vérifiez les données et réessayez !');
-        // Affiche un message d'erreur dans le champ verifierDonnées
-        let verifierDonnees = document.getElementById(`verifierDonnees`)
-        verifierDonnees.textContent = `Vérifiez les données et réessayez !`;
-
-        // Animation d'erreur pour verifierDonnées
-        verifierDonnees.animate([
+    
+    // Animation d'erreur pour verifierDonnées
+    verifierDonnees.animate(
+        [
             { opacity: 1 },
             { opacity: 0 },
             { opacity: 1 }
-        ], {
+        ],
+        {
             duration: 1000, // durée
             iterations: 2 // répétitions
         });
 
+    // Vérifie si les champs email et mot de passe sont remplis
+    if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
+
+        // Affiche un message d'erreur dans le champ verifierDonnées
+        verifierDonnees.textContent = "Veuillez remplir tous les champs.";
+
+        return;
     } else {
         // Si tout est bon, crée la constante user
         const user = {
             email: emailInput.value,
             password: passwordInput.value
         };
-        console.log(user);
 
         // Déclare la variable classefetch pour qu'elle soit accessible dans tout le fichier
         fetch('http://localhost:5678/api/users/login', {
@@ -61,16 +67,20 @@ loginForm.addEventListener('submit', function (event) {
                 } else {
                     // Sinon, affiche une erreur
                     console.log("Erreur dans l'identifiant ou le mot de passe");
-                    verifierDonnees.textContent = `Vérifiez les données et réessayez !`;
+                    verifierDonnees.textContent = "Identifiants incorrects. Veuillez réessayer.";
+
                 }
             })
             .catch(e => {
                 console.log("Il y a eu un problème avec votre opération fetch : " + e.message);
+                verifierDonnees.textContent = "Identifiants incorrects. Veuillez réessayer.";
             });
+            
     }
 
     function checkDonnees(serverData) {
         // Renvoie true si les données sont ok, sinon renvoie false
+        console.log("checkDonnees: ", serverData.token);
         return serverData.token;
     }
-})
+});
